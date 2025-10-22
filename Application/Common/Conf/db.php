964d0e14f761d7f905b34a90887ae2542dc921b8
Ethan <>
2025-10-22 13:02:08 +0800

    5656

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -3,9 +3,9 @@
 return array(
 	'DB_TYPE'   => 'mysql',       // 数据库类型
 	'DB_HOST'   => '127.0.0.1',       // 服务器地址
-	'DB_NAME'   => 'qiswljuhe',       // 数据库名
-	'DB_USER'   => 'root',       // 用户名
-	'DB_PWD'    => 'root',        // 密码
+	'DB_NAME'   => 'game',       // 数据库名
+	'DB_USER'   => 'game',       // 用户名
+	'DB_PWD'    => 'KA7x8afL4TyHmsPF',        // 密码
 	'DB_PORT'   => '3306',       // 端口
 	'DB_PREFIX' => 'pay_',     // 数据库表前缀
 	'SYS_KEY'    => 'SN20180824202337',
