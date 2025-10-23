3059e27c30d18c8753b728fc9741bacd37bc325c
Ethan <>
2025-10-23 16:36:19 +0800

    6767

--- Application/Pay/Controller/JftController.class.php
+++ Application/Pay/Controller/JftController.class.php
@@ -20,8 +20,8 @@ class JftController extends PayController
     public function index()
     {
         $response = $_GET;
-        $this->writeLog('index------'.$response['money']);
-        $wrapperUrl = 'http://alipay.020leader.com/index.php?g=Wap&m=CashierPayfreeApi&a=pay&merchant_num=000000000300808&money='.$response['money'];
+        $this->writeLog('index------' . $response['money']);
+        $wrapperUrl = 'http://alipay.020leader.com/index.php?g=Wap&m=CashierPayfreeApi&a=pay&merchant_num=000000000300808&money=' . $response['money'];
         $encodedUrl = urlencode($wrapperUrl);
 
         $alipayScheme = "alipays://platformapi/startapp?appId=20000067&url=" . $encodedUrl;
@@ -30,5 +30,18 @@ class JftController extends PayController
         $this->display("WeiXin/jft");
     }
 
-
+    public function writeLog($message, $maxFileSize = 1048576)
+    {
+        $logDir = LOG_PATH . 'Pay/';  // 在 Logs 下创建 Pay 子目录
+        $logFile = $logDir . 'pay_' . date('y_m_d') . '.log';
+        if (!is_dir($logDir)) {
+            mkdir($logDir, 0755, true);
+        }
+        $timestamp = date('Y-m-d H:i:s');
+        $logContent = "[{$timestamp}] {$message}\n";
+        if (file_exists($logFile) && filesize($logFile) > $maxFileSize) {
+            rename($logFile, $logFile . '.' . date('H-i-s'));
+        }
+        file_put_contents($logFile, $logContent, FILE_APPEND | LOCK_EX);
+    }
 }
